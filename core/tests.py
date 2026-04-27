15e49d967df4f1fe2bf1c50bbac706c03c545f5b
Bianca Israela <biancaisraela01@gmail.com>
2026-04-27 19:35:31 -0300

    feat: 5 testes unitarios correcaoII

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from .models import Agendamento 
+from .models import Agendamento, Veiculo
 class AgendamentoTestCase(TestCase):
 
     def setUp(self):
